130beeb2048bd86f06c3532297b3d59f7e270aa1
fantasticjimmy <dboyjimmy@gmail.com>
2024-06-17 00:43:16 -0400

    Initial commit

---
+++ apps/clover_health/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'clover_health',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/clover_health',
+};
