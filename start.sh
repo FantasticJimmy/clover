4fcf18dfc71c194c921b7c2816ac66b68f047dbd
fantasticjimmy <dboyjimmy@gmail.com>
2024-06-24 01:25:44 -0400

    chore: change start up script

--- start.sh
+++ start.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
+NODE_VERSION=$(node -v)
+REQUIRED_VERSION="v18.17.1"
 
-npm i
-nx build backend
-docker compose build
-docker compose up -d
+if [[ $NODE_VERSION == $REQUIRED_VERSION ]]; then
+  echo "Node version is $NODE_VERSION, proceeding with the script."
+  
+  npm install
+  nx build backend
+  docker compose build
+  docker compose up -d
 
-# Unfinished work for communication between convert and backend, so we are launching convert in local instead of container
-cd apps/convert/
-source venv/bin/activate
-python main.py
+  # Unfinished work for communication between convert and backend, so we are launching convert in local instead of container
+  cd apps/convert/
+  source venv/bin/activate
+  python main.py
+else
+  echo "Node version is $NODE_VERSION, required version is $REQUIRED_VERSION. Exiting."
+  exit 1
+fi
