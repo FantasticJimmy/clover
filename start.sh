fba29571ed7e64eb2cb0191e00ed137b6e9da066
fantasticjimmy <dboyjimmy@gmail.com>
2024-06-24 01:28:47 -0400

    chore: checking for docker

--- start.sh
+++ start.sh
@@ -2,9 +2,23 @@
 NODE_VERSION=$(node -v)
 REQUIRED_VERSION="v18.17.1"
 
+check_docker() {
+  if ! command -v docker &> /dev/null; then
+    echo "Docker is not installed. Exiting."
+    exit 1
+  fi
+
+  if ! docker info &> /dev/null; then
+    echo "Docker is not running. Please start Docker and try again. Exiting."
+    exit 1
+  fi
+}
+
 if [[ $NODE_VERSION == $REQUIRED_VERSION ]]; then
   echo "Node version is $NODE_VERSION, proceeding with the script."
   
+  check_docker
+
   npm install
   nx build backend
   docker compose build
